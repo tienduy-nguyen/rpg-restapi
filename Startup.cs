fb08b84520127b947777ffce235339d9e5a86164
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-23 12:09:18 +0100

    Add Swagger with Swashbuckle

--- Startup.cs
+++ Startup.cs
@@ -10,42 +10,48 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace Rpg_Restapi
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+namespace Rpg_Restapi {
+  public class Startup {
+    public Startup (IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices (IServiceCollection services) {
+      services.AddControllers ();
+      services.AddSwaggerGen (c => {
+        c.SwaggerDoc ("v1", new OpenApiInfo {
+          Title = "Role Playing Game API",
+            Version = "v1",
+            Description = "Documentation for RPG API"
+        });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+      app.UseSwagger ();
+      app.UseSwaggerUI (c => {
+        c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Rpg api v1");
+        c.RoutePrefix = string.Empty;
+      });
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
+      }
+
+      app.UseHttpsRedirection ();
+
+      app.UseRouting ();
+
+      app.UseAuthorization ();
+
+      app.UseEndpoints (endpoints => {
+        endpoints.MapControllers ();
+      });
+    }
+  }
 }
