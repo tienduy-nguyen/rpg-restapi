ae17b90ccbfc81332056d2ededd7076d682d2c62
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-23 13:02:52 +0100

    Add JsonPatch to handle partial json input

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Rpg_Restapi.Models;
 
@@ -7,10 +8,38 @@ namespace Rpg_Restapi.Controllers {
   [ApiController]
   public class CharactersController : ControllerBase {
     private static Character _knight = new Character ();
+    private static List<Character> _characterList = new List<Character> {
+      new Character (),
+      new Character { Id = 1, Name = "Same" },
+      new Character { Id = 2, Name = "Paul" },
+      new Character { Id = 2, Name = "John" }
+    };
 
     [HttpGet]
-    public ActionResult<IEnumerable<Character>> Get () {
-      return Ok (_knight);
+    public ActionResult<IEnumerable<Character>> GetAll () {
+      return Ok (_characterList);
+    }
+
+    [HttpGet ("{id}")]
+    public ActionResult<Character> GetCharacterById (int id) {
+      return Ok (_characterList.FirstOrDefault (c => c.Id == id));
+    }
+
+    [HttpPost]
+    public ActionResult<Character> CreateCharacter (Character newCharacter) {
+      var check = _characterList.FirstOrDefault (c => c.Id == newCharacter.Id);
+      if (check != null) return Conflict (new { message = $"Character with id '{newCharacter.Id}' already existed!" });
+      _characterList.Add (newCharacter);
+      return Ok (newCharacter);
+    }
+
+    [HttpPut ("{id}")]
+    public IActionResult UpdateCharacter (int id, Character character) {
+      var check = _characterList.FirstOrDefault (c => c.Id == id);
+      if (check == null) return NotFound ();
+      int index = _characterList.FindIndex (c => c.Id == id);
+      _characterList[index] = character;
+      return NoContent ();
     }
   }
 }
