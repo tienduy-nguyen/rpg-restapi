cd5e53a362d87d0924cc71c0882c5650067a2973
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-24 22:19:11 +0100

    Create migration to integrate user model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,6 +5,7 @@ namespace Rpg_Restapi.Data {
   public class DataContext : DbContext {
     public DataContext (DbContextOptions<DataContext> options) : base (options) { }
     public DbSet<Character> Characters { get; set; }
+    public DbSet<User> Users { get; set; }
 
   }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,7 @@ namespace Rpg_Restapi.Models {
     public string Username { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
-    public List<Character> Characters { get; set; }
-    public string Role { get; set; }
+    // public List<Character> Characters { get; set; }
+    // public string Role { get; set; }
   }
 }
