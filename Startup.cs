99383191f9e9927c3cf1ea2892413a7d06e154ca
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-25 20:09:52 +0100

    Add Header authentication for swagger UI

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -29,18 +30,7 @@ namespace Rpg_Restapi {
     public void ConfigureServices (IServiceCollection services) {
       services.AddControllers ()
         .AddNewtonsoftJson (options => options.UseMemberCasing ());
-
-      /* Config for Jwt */
-      services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer (options => {
-          options.TokenValidationParameters = new TokenValidationParameters {
-          ValidateIssuerSigningKey = true,
-          IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
-          .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
-          ValidateIssuer = false,
-          ValidateAudience = false
-          };
-        });
+      services.AddHttpContextAccessor ();
 
       /* Handle json (for JsonPath) */
       services.AddDbContext<DataContext> (options => {
@@ -62,7 +52,40 @@ namespace Rpg_Restapi {
             Version = "v1",
             Description = "Documentation for RPG API"
         });
+        c.AddSecurityDefinition ("Bearer", new OpenApiSecurityScheme () {
+          Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        });
+        c.AddSecurityRequirement (new OpenApiSecurityRequirement {
+          {
+            new OpenApiSecurityScheme {
+              Reference = new OpenApiReference {
+                Type = ReferenceType.SecurityScheme,
+                  Id = "Bearer"
+              }
+            },
+            new string[] { }
+
+          }
+        });
+
       });
+
+      /* Config for Jwt */
+      services.AddAuthentication (JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer (options => {
+          options.TokenValidationParameters = new TokenValidationParameters {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = new SymmetricSecurityKey (Encoding.ASCII
+          .GetBytes (Configuration.GetSection ("AppSettings:Token").Value)),
+          ValidateIssuer = false,
+          ValidateAudience = false
+          };
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -80,8 +103,8 @@ namespace Rpg_Restapi {
 
       app.UseRouting ();
 
-      app.UseAuthorization ();
       app.UseAuthentication ();
+      app.UseAuthorization ();
 
       app.UseEndpoints (endpoints => {
         endpoints.MapControllers ();
