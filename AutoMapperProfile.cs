9f323665cabc44b8e9df2aea2c1ae3a86f46ef8d
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-27 14:30:03 +0100

    Complet UserService

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -6,17 +6,24 @@ using Rpg_Restapi.Models;
 namespace Rpg_Restapi {
   public class AutoMapperProfile : Profile {
     public AutoMapperProfile () {
+      /* Map character */
       CreateMap<Character, GetCharacterDto> ()
         .ForMember (dto => dto.Skills, c => c.MapFrom (c => c.CharacterSkills.Select (cs => cs.Skill)));
       CreateMap<AddCharacterDto, Character> ();
       CreateMap<UpdateCharacterDto, Character> ();
       CreateMap<Character, UpdateCharacterDto> ();
+
+      /* Map User */
+      CreateMap<User, GetUserDto> ();
+      CreateMap<UpdateUserDto, User> ();
+
       CreateMap<AddWeaponDto, Weapon> ();
       CreateMap<Weapon, GetWeaponDto> ();
       CreateMap<AddCharacterSkillDto, CharacterSkill> ();
       CreateMap<Skill, GetSkillDto> ();
       CreateMap<Character, HighscoreDto> ();
 
+      /* Map Skill */
       CreateMap<Skill, GetSkillDto> ();
       CreateMap<AddSkillDto, Skill> ();
       CreateMap<UpdateSkillDto, Skill> ();
--- Services/UserService/UserService.cs
+++ Services/UserService/UserService.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Rpg_Restapi.Data;
 using Rpg_Restapi.Dtos;
 using Rpg_Restapi.Models;
@@ -16,32 +18,137 @@ namespace Rpg_Restapi.Services {
 
     public async Task<ServiceResponse<List<GetUserDto>>> GetAllUsers () {
       ServiceResponse<List<GetUserDto>> response = new ServiceResponse<List<GetUserDto>> ();
-      // var userList = _context.Users.
+      try {
+        var userList = await _context.Users.ToListAsync ();
+        response.Data = userList.Select (u => _mapper.Map<GetUserDto> (u)).ToList ();
+
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
       return response;
     }
 
     public async Task<ServiceResponse<GetUserDto>> GetUserById (int id) {
-      throw new System.NotImplementedException ();
+      ServiceResponse<GetUserDto> response = new ServiceResponse<GetUserDto> ();
+
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Id == id);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with id {id} not found!";
+          return response;
+        }
+        response.Data = _mapper.Map<GetUserDto> (user);
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
+
     }
 
     public async Task<ServiceResponse<GetUserDto>> GetUserByUsername (string username) {
-      throw new System.NotImplementedException ();
+      ServiceResponse<GetUserDto> response = new ServiceResponse<GetUserDto> ();
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Username == username);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with username {username} not found!";
+          return response;
+        }
+        response.Data = _mapper.Map<GetUserDto> (user);
+        return response;
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
     }
 
     public async Task<ServiceResponse<GetUserDto>> UpdateUser (int id, UpdateUserDto updateUserDto) {
-      throw new System.NotImplementedException ();
+      ServiceResponse<GetUserDto> response = new ServiceResponse<GetUserDto> ();
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Id == id);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with id {id} not found!";
+          return response;
+        }
+        var updateUser = _mapper.Map<User> (updateUserDto);
+        _context.Entry (updateUser).State = EntityState.Modified;
+        await _context.SaveChangesAsync ();
+        response.Data = _mapper.Map<GetUserDto> (updateUser);
+        return response;
+
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
+
     }
 
     public async Task<ServiceResponse<GetUserDto>> UpdateUser (string username, UpdateUserDto updateUserDto) {
-      throw new System.NotImplementedException ();
+
+      ServiceResponse<GetUserDto> response = new ServiceResponse<GetUserDto> ();
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Username == username);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with username {username} not found!";
+          return response;
+        }
+        var updateUser = _mapper.Map<User> (updateUserDto);
+        _context.Entry (updateUser).State = EntityState.Modified;
+        await _context.SaveChangesAsync ();
+        response.Data = _mapper.Map<GetUserDto> (updateUser);
+        return response;
+
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
     }
 
     public async Task<ServiceResponse<GetUserDto>> DeleteAccount (int id) {
-      throw new System.NotImplementedException ();
+      ServiceResponse<GetUserDto> response = new ServiceResponse<GetUserDto> ();
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Id == id);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with id {id} not found!";
+          return response;
+        }
+        _context.Users.Remove (user);
+        await _context.SaveChangesAsync ();
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
     }
 
     public async Task<ServiceResponse<List<GetUserDto>>> DeleteUser (int id) {
-      throw new System.NotImplementedException ();
+      ServiceResponse<List<GetUserDto>> response = new ServiceResponse<List<GetUserDto>> ();
+      try {
+        User user = await _context.Users.FirstOrDefaultAsync (u => u.Id == id);
+        if (user == null) {
+          response.Success = false;
+          response.Message = $"User with id {id} not found!";
+          return response;
+        }
+        _context.Users.Remove (user);
+        await _context.SaveChangesAsync ();
+
+        var newUserList = await _context.Users.ToListAsync ();
+        response.Data = newUserList.Select (u => _mapper.Map<GetUserDto> (u)).ToList ();
+      } catch (System.Exception ex) {
+        response.Success = false;
+        response.Message = ex.Message;
+      }
+      return response;
     }
 
   }
