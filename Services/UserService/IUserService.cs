f454fd3ec9aff64d96dae0440292f0eb709f686e
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-27 11:01:43 +0100

    Update UserService Interface

--- Services/UserService/IUserService.cs
+++ Services/UserService/IUserService.cs
@@ -1,12 +1,18 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Rpg_Restapi.Dtos;
 using Rpg_Restapi.Models;
 
 namespace Rpg_Restapi.Services.UserService {
   public interface IUserService {
-    // Get usersList -> admin
-    // Get user detail
-    // Update user
-    // Delete user
+
+    Task<ServiceResponse<List<GetUserDto>>> GetAllUsers ();
+    Task<ServiceResponse<GetUserDto>> GetUserById (int id);
+    Task<ServiceResponse<GetUserDto>> GetUserByUsername (string username);
+    Task<ServiceResponse<GetUserDto>> UpdateUser (int id, UpdateUserDto updateUserDto);
+    Task<ServiceResponse<GetUserDto>> UpdateUser (string username, UpdateUserDto updateUserDto);
+    Task<ServiceResponse<GetUserDto>> DeleteAccount (int id);
+    Task<ServiceResponse<List<GetUserDto>>> DeleteUser (int id);
+
   }
 }
