fe9284271c66f24c0a586687c462f726f8119399
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-27 15:02:11 +0100

    Fix duplicate reference

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Rpg_Restapi.Dtos;
 using Rpg_Restapi.Models;
