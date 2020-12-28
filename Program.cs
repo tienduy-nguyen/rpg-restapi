9aa2ca5a72408c3daf5d3026f5686dbe66306196
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-12-28 14:50:52 +0100

    Try using port in Program for prod

--- Program.cs
+++ Program.cs
@@ -28,13 +28,26 @@ namespace Rpg_Restapi {
     }
 
     public static IHostBuilder CreateHostBuilder (string[] args) {
-      // var port = Environment.GetEnvironmentVariable ("PORT");
       return Host.CreateDefaultBuilder (args)
-        .ConfigureWebHostDefaults (webBuilder => {
+        .ConfigureWebHostDefaults ((webBuilder) => {
+          webBuilder.UseUrls ($"http://+:{HostPort}");
+          webBuilder.UseKestrel ();
           webBuilder.UseStartup<Startup> ();
         });
+      // return Host.CreateDefaultBuilder (args)
+      //   .ConfigureWebHostDefaults (webBuilder => {
+      //     webBuilder.UseStartup<Startup> ();
+      //   });
 
     }
+
+    private static bool IsDevelopment =>
+      Environment.GetEnvironmentVariable ("ASPNETCORE_ENVIRONMENT") == "Development";
+
+    public static string HostPort =>
+      IsDevelopment ?
+      "5000" :
+      Environment.GetEnvironmentVariable ("PORT");
 
   }
 }
